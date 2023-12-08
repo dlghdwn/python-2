974b1ba68fbcdf8934acdabf480de90d19fd1f37
Leehongju <dlghdwn0727@gmail.com>
2023-12-08 17:42:56 +0900

    시각화 처리

--- 15_2주차.py
+++ 15_2주차.py
@@ -24,7 +24,7 @@ import matplotlib.pyplot as plt
 #plt.colorbar()
 
 #==============히스토그램 도수 분표========= 부족
-#import numpy as np
+import numpy as np
 
 #rn = np.randaom.
 
@@ -42,8 +42,8 @@ import matplotlib.pyplot as plt
 #plt.hist(rn,  bins=10, label="def", alpha=0.5, histtype="step")
 
 #===========파이챠트 그리기====================
-rate = [30, 40, 20, 10]
-labels = ["Alpha", "Beta", "Gamma", "Delta"]
+#rate = [30, 40, 20, 10]
+#labels = ["Alpha", "Beta", "Gamma", "Delta"]
 
 #plt.pie(rate)
 #plt.pie(rate, labels=labels)
@@ -68,18 +68,143 @@ labels = ["Alpha", "Beta", "Gamma", "Delta"]
 # 사용가능한 스타일 키
 #print(plt.style.available)
 
-#plt.plot([2,3,6,7,10], [1,4,5,8,9])
 #plt.style.use("bmh")
 #plt.style.use("ggplot")
 #plt.style.use("classic")
 #plt.style.use("Solarize_Light2")
 #plt.style.use("dark_background")
 #plt.style.use("fast")
-#plt.style.use("bmh")
-#plt.style.use("bmh")
-#plt.style.use("bmh")
-#plt.style.use("bmh")
+
+#plt.plot([2,3,6,7,10], [1,4,5,8,9])
+
+#===========포맷 설정(일단 패스)====================
+
+#패널 사이즈 적용
+#plt.rcParams['figure.figsize'] = (1, 2)
+#plt.rcParams['figure.figsize'] = (10, 10)
+
+#전체 폰트 사이즈
+#plt.rcParams['font.size'] = 15
+#plt.rcParams['font.size'] = 1
+
+#선 두께
+#plt.rcParams['lines.linewidth'] = 5
+#plt.rcParams['lines.linewidth'] = 10
+
+#선 스타일
+#plt.rcParams['lines.linestyle'] = '--'
+
+#눈금 설정
+ # 위 눈금 설정
+#plt.rcParams['xtick.top'] = True
+
+ # 오른 눈금 설정
+#plt.rcParams['ytick.right'] = True
+
+ # 안쪽으로 눈금 설정
+#plt.rcParams['xtick.direction'] = 'in'
+#plt.rcParams['ytick.direction'] = 'in'
+
+ # 눈금 길이
+#plt.rcParams['ytick.major.size'] = 12
+
+ # 세부 눈금
+#plt.rcParams['xtick.minor.visible'] = True
+
+#plt.plot([2,3,6,7,10], [1,4,5,8,9])
+#===========객체 활용====================
+# 기본 예제
+#fig, ax = plt.subplots()
+#ax = fig.add_axes([0, 0, 0, 0]) #왼, 밑, 두께 높이
+#ax = fig.add_axes([1, 1, 0, 0]) 
+#ax = fig.add_axes([1, 1, 1, 1])
+
+#다중 패널 객체 생성
+#fig, ax = plt.subplots(1, 1)
+#fig, ax = plt.subplots(1, 2)
+#fig, ax = plt.subplots(2, 1)
+#fig, ax = plt.subplots(2, 2)
+
+#다중 패널 그래프 생성
+""" x = [1,4,5,8,9]
+y1 = [2,3,6,7,10]
+
+fig, ax = plt.subplots(2, 2)
+
+ax[0][0].plot(x, y1)
+# ~
+ax[1][1].plot(x, y1) """
 
 
+#축 공유
+""" fig, ax = plt.subplots(2, 2, sharex=True)
+fig, ax = plt.subplots(2, 2, sharey=True) """
 
+#============y축 동시 표시=============
+# 사용 데이터
+""" x = [1,4,5,8,9]
+
+y1 = [2,3,6,7,10]
+y2 = [10,8,6,4,2]
+
+fig, ax1 = plt.subplots()
+ax1.set_xlabel("X-Data")
+ax1.set_ylabel("Y1")
+ax1.plot(x, y1)
+
+ax2 = ax1.twinx()
+ax2.set_ylabel("Y2")
+ax2.plot(x, y2)
+
+#ax1 / ax2 개별 색상 설정
+ax1.plot(x, y1, color="C1")
+ax2.plot(x, y2, color="C2")
+
+#ax1 / ax2 개별 라벨 출력
+ax1.plot(x, y1, color="C1", label="y1 Data")
+ax1.legend(loc="upper right")
+
+ax2.plot(x, y2, color="C2", label="y2 Data")
+ax2.legend(loc="lower right") """
+
+#===========이종 그래프 출력===============
+""" x = [1,4,5,8,9]
+y1 = [2,3,6,7,10]
+y2 = [2,3,6,7,10]
+
+fig, ax1 = plt.subplots()
+
+ax1.plot(x, y1, "-o", color="C1")
+ax1.set_xlabel("X")
+ax1.set_ylabel("Ydata")
+
+ax2 = ax1.twinx()
+ax2.bar(x, y2, color="C2")
+ax2.set_ylabel("Y2data")
+
+
+#라벨
+ax1.plot(x, y1, "-o", color="C1", label="XData")
+# ~
+ax2.bar(x, y2, color="C2", label="YData")
+# ~
+
+ax1.legend()
+ax2.legend() """
+
+
+#==========다중 그래프 출력==================
+# 사용데이터
+""" x1 = [2,3,6,7,10]
+x2 = [1,4,5,8,9]
+
+y1 = [1,4,5,8,9]
+y2 = [2,4,6,8,10]
+
+plt.subplot(2, 1, 1)    # 1set
+plt.plot(x1, y1, "o-")
+
+plt.subplot(2, 1, 2)
+plt.plot(x2, y2, ".-")
+ """
 plt.show()
